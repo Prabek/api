95ef6718484f2c97d21f0ec7c79167bb3c18cde2
prabek <prabrk79@gmail.com>
2018-06-13 17:20:51 +0545

    seeder part in the Resource and Product

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,5 +5,12 @@ use Faker\Generator as Faker;
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
         //
+    'name' => $faker->word,
+    'detail' => $faker->paragraph,
+    'price' => $faker->numberBetween(100,10000),
+    'discount' => $faker->numberBetween(1,30),
+    'stock' => $faker->randomDigit
+
+
     ];
 });
